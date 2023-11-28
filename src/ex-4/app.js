4ebbf1c62f83148200855de2cb1bc525b0a96202
RebecaMFep <martinezfeliperebeca@gmail.com>
2023-11-28 21:56:56 +0100

    created ex-4

---
+++ src/ex-4/app.js
@@ -0,0 +1,10 @@
+// Escribe un programa que pida dos números y escriba “La suma de <numero-uno> con <numero-dos> es <resultado>”.
+
+// 1. Iniciar app 
+// 2. Crear una sección donde se va a mostar el mensaje 
+// 3. Crear dato de los dos números
+// 4 Crear el dato (frase)
+// 5. Crear el dato que recoge la frase 
+// 6. Declarar el elemento "escuchador"
+// 7. Crear una function 
+// 8. Asignar método
